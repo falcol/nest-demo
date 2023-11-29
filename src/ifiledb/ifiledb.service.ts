251f5449894c272d4bffcfe3457566d0d4f6636b
rikkei-dinhngoc <ngocbd@rikkeisoft.com>
2023-11-29 17:31:17 +0700

    fix show err list and issuse 6

--- src/ifiledb/ifiledb.service.ts
+++ src/ifiledb/ifiledb.service.ts
@@ -47,6 +47,8 @@ export class IfiledbService {
 		const orderMap = {};
 		const nameMap = {};
 		const groupedData = {};
+		const oldQuery = { from: '', id: '' };
+		const query = [];
 
 		// Check encoding utf8
 		// Trường hợp file CSV end code không phải UTF8 thì bắn lỗi
@@ -67,31 +69,39 @@ export class IfiledbService {
 						reject(error);
 					}
 				})
-				.on('data', (row) => {
+				.on('data', async (row) => {
 					rowCount++;
 					const data = new CreateIfiledbDto(row);
 					try {
 						// Trường hợp header hoặc 1 row nào đó không đủ 8 collumn hoặc thừa ra (bất kể 1 row nào) thì cũng bắn message
-						this.checkRow(data, headerLength, rowCount);
+						this.checkRow(data, headerLength, rowCount, errorRows);
 						// Validate bắt buộc nhập cột name,from trong file. Trường nào không nhập thì bắn message cấu trúc {tên hạng mục} require
 						this.checkRequiredFields(data, fieldRequired, rowCount, errorRows);
 						//  nếu trùng 1 bộ id,from thì order k được trùng lặp. Nếu trùng thì bắn lỗi  Trường hợp trùng lặp thì bắn lỗi "order duplicate"
-						this.checkOrder(data, orderMap, rowCount);
+						this.checkOrder(data, orderMap, rowCount, errorRows);
 						// Gom data lại theo Set id, ngày from validate trùng data nếu cùng ID, From mà bộ 3 key: s1_name,s2_name,color_name trùng nhau thì báo message "Duplicate"
-						this.checkNames(data, nameMap, rowCount);
+						this.checkNames(data, nameMap, rowCount, errorRows);
 						// Valid ngày TO (Ngày TO chỉ được phép set cho set cho order = max(order) cuối cùng)
 						this.groupData(data, groupedData);
-
+						if (data.id != oldQuery.id || oldQuery.from != data.from) {
+							oldQuery.id = data.id;
+							oldQuery.from = data.from;
+							query.push(oldQuery);
+						}
 						results.push(data);
 					} catch (error) {
 						bufferStream.destroy();
 						reject(error);
 					}
 				})
-				.on('end', () => {
+				.on('end', async () => {
 					try {
+						await Promise.all(query.map((data) => this.groupDataDb(groupedData, data.id, data.from)));
 						// Valid ngày TO (Ngày TO chỉ được phép set cho set cho order = max(order) cuối cùng)
-						this.validateGroupedData(groupedData);
+						await this.validateGroupedData(groupedData, errorRows);
+						if (errorRows.length > 0) {
+							throw new HttpException({ error: errorRows, status: HttpStatus.BAD_REQUEST }, HttpStatus.BAD_REQUEST);
+						}
 						resolve(results);
 					} catch (error) {
 						reject(error);
@@ -139,43 +149,33 @@ export class IfiledbService {
 		}
 	}
 
-	checkOrder(row: any, orderMap: object, rowCount: number) {
+	checkOrder(row: any, orderMap: object, rowCount: number, errorRows: string[]) {
 		const key = `${row.id}-${row.from}-${row.order}`;
 		if (orderMap[key]) {
-			throw new HttpException(`Order duplicate in row from ${orderMap[key]} and ${rowCount}`, HttpStatus.BAD_REQUEST);
+			errorRows.push(`Order duplicate in row from ${orderMap[key]} and ${rowCount}`);
 		}
 		orderMap[key] = rowCount;
 	}
 
-	checkRow(row: any, headerLength: number, rowCount: number) {
+	checkRow(row: any, headerLength: number, rowCount: number, errorRows: string[]) {
 		const rowValues = Object.values(row);
 		if (rowValues.length !== headerLength) {
-			throw new HttpException(
-				`Row ${rowCount} in the CSV file does not have the same number of columns as there are headers.`,
-				HttpStatus.BAD_REQUEST,
-			);
+			errorRows.push(`Row ${rowCount} in the CSV file does not have the same number of columns as there are headers.`);
 		}
 	}
 
-	checkRequiredFields(row: any, fieldRequired: string[], rowCount: number, errorRows: any[]) {
+	checkRequiredFields(row: any, fieldRequired: string[], rowCount: number, errorRows: string[]) {
 		fieldRequired.forEach((field) => {
 			if (!row[field]) {
-				errorRows.push(`Row ${rowCount} must be required field ${field}`);
+				errorRows.push(`Row ${rowCount} must be required field: ${field}`);
 			}
 		});
-
-		if (errorRows.length != 0) {
-			throw new HttpException(errorRows, HttpStatus.BAD_REQUEST);
-		}
 	}
 
-	checkNames(row: any, nameMap: object, rowCount: number) {
+	checkNames(row: any, nameMap: object, rowCount: number, errorRows: string[]) {
 		const key = `${row.id}-${row.from}-${row.s1_name}-${row.s2_name}-${row.color_name}`;
 		if (nameMap[key]) {
-			throw new HttpException(
-				`Duplicate s1_name, s2_name, color_name in row from ${nameMap[key]} and ${rowCount}`,
-				HttpStatus.BAD_REQUEST,
-			);
+			errorRows.push(`Duplicate s1_name, s2_name, color_name in row from ${nameMap[key]} and ${rowCount}`);
 		}
 		nameMap[key] = rowCount;
 	}
@@ -188,8 +188,24 @@ export class IfiledbService {
 		groupedData[idFromKey].push(row);
 	}
 
-	validateGroupedData(groupedData: object) {
-		const errors = [];
+	async groupDataDb(groupedData: object, id: string, from: string) {
+		const dataInDB = await this.ifileRepository.find({ where: { id: id, from: from } });
+		dataInDB.forEach((data) => {
+			const idFromKey = `${data.id}-${data.from}`;
+			if (!groupedData[idFromKey]) {
+				groupedData[idFromKey] = [];
+			}
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const { pk, ...result } = data;
+			const resultDto = new CreateIfiledbDto(result);
+			if (!groupedData[idFromKey].some((obj) => JSON.stringify(obj) === JSON.stringify(resultDto))) {
+				groupedData[idFromKey].push(resultDto);
+			}
+		});
+		return groupedData;
+	}
+
+	async validateGroupedData(groupedData: object, errors: string[]) {
 		for (const [key, group] of Object.entries(groupedData)) {
 			const sortedGroup = group.sort((a, b) => a.order - b.order);
 			const lastRow = sortedGroup[sortedGroup.length - 1];
@@ -202,9 +218,6 @@ export class IfiledbService {
 					errors.push(errStr);
 				}
 			}
-		}
-		if (errors.length > 0) {
-			throw new HttpException({ errors: errors, status: HttpStatus.BAD_REQUEST }, HttpStatus.BAD_REQUEST);
 		}
 	}
 
